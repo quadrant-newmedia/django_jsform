e6fe323a092b476815080055e12ff44832090884
Alex Fischer <alex@quadrant.net>
2020-05-25 15:27:05 -0600

    Added missing files to package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django_jsform",
-    version="1.0.0",
+    version="1.0.1-alpha",
     author="Alex Fischer",
     author_email="alex@quadrant.net",
     description="Django integration for jsform",
